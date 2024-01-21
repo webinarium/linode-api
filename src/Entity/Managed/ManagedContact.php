43615c410ecaa1df614ad907e6303e2786df1b95
Artem Rodygin <webinarium@users.noreply.github.com>
2024-01-21 17:29:47 +1300

    [skip ci] Copyright update

--- src/Entity/Managed/ManagedContact.php
+++ src/Entity/Managed/ManagedContact.php
@@ -1,13 +1,13 @@
 <?php
 
-//----------------------------------------------------------------------
+// ---------------------------------------------------------------------
 //
-//  Copyright (C) 2018 Artem Rodygin
+//  Copyright (C) 2018-2024 Artem Rodygin
 //
 //  You should have received a copy of the MIT License along with
 //  this file. If not, see <http://opensource.org/licenses/MIT>.
 //
-//----------------------------------------------------------------------
+// ---------------------------------------------------------------------
 
 namespace Linode\Entity\Managed;
 
